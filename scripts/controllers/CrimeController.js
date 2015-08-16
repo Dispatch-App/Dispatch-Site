fc8659e7f2e429e1142611cfaf419cb2bbce91cb
Daniel <dan_pc@live.com>
2015-08-15 23:16:58 -0400

    Working maps with angular, added crime service

--- scripts/controllers/CrimeController.js
+++ scripts/controllers/CrimeController.js
@@ -1,6 +1,26 @@
-var app = angular.module('dispatch', []);
+angular.module('dispatch', ['ngMap'])
 
-app.controller('crimeController', ['$scope',
-  function($scope) {
-    $scope.ham = "hammy";
+.factory('crimes', function($http) {
+    return {
+        get: function() {
+            return $http({
+                    url: 'https://dispatch-danielchristopher1.c9.io/getCrimes',
+                    method: "GET",
+                    params: {
+                        longitude: 23,
+                        latitude: 24,
+                        range: 10
+                    }
+                 });
+        }
+    };
+})
+
+.controller('crimeController', ['$scope', 'crimes', function($scope, $crimes) {
+
+    $scope.$on('mapInitialized', function(event, map) {
+        $crimes.get().then(function(data) {
+            console.log(data);
+        });
+    });
 }]);
--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,45 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 17
-  });
-
-  getCurrentLocation({
-      success: function(location) {
-         var loc = {lat: location.coords.latitude, lng: location.coords.longitude};
-
-         new google.maps.Marker({
-            map: map,
-            position: loc,
-            title: "Me"
-         });
-
-         map.panTo(loc);
-      },
-
-      error: function(e) {
-          console.log(e);
-          alert("Please enable geolocation");
-      }
-  });
-}
-
-function getCurrentLocation(callbacks) {
-    if(navigator.geolocation) {
-        return navigator.geolocation.getCurrentPosition(
-            function(position) {
-                callbacks.success(position);
-            },
-
-            function(e) {
-                callbacks.error(e);
-            }
-        );
-    }
-}
+// $(document).ready(function() {
+//     getCrimes({
+//         success: function(data) {
+//             console.log(data);
+//         },
+//
+//         error: function(error) {
+//             console.log(error);
+//         }
+//     });
+// });
+//
+// function getCrimes(callback) {
+//     $.ajax({
+//         method: 'GET',
+//         url: 'https://dispatch-danielchristopher1.c9.io/getCrimes',
+//         contentType: 'application/json',
+//         longitude: 23,
+//         latitude: 24,
+//         radius: 10
+//         },
+//
+//         function(data) {
+//             callback.success(data);
+//         },
+//
+//         function (error) {
+//             callback.error(error);
+//         }
+//     );
+// }
+//
+// function getCurrentLocation(callbacks) {
+//     if(navigator.geolocation) {
+//         return navigator.geolocation.getCurrentPosition(
+//             function(position) {
+//                 callbacks.success(position);
+//             },
+//
+//             function(e) {
+//                 callbacks.error(e);
+//             }
+//         );
+//     }
+// }
